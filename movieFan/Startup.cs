f0ded83c7d22553366e38e99f31764a4bd65d871
robert bowman <bowcorp@gmail.com>
2017-03-12 05:05:03 -0400

    Add project files.

---
+++ movieFan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(movieFan.Startup))]
+namespace movieFan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
